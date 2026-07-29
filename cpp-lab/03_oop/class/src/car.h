babdae0393561319bab408f32e85ceb9df45b9a8
Miao Jiang <miao@zeustellus.com>
2026-07-29 16:42:41 -0700

    start OOP

---
+++ cpp-lab/03_oop/class/src/car.h
@@ -0,0 +1,17 @@
+#pragma once 
+class Car{
+
+private:
+    float fuel;
+    float speed;
+    int passengers;
+
+
+public:
+    void FillFuel(float amount);
+    void Accelerate();
+    void Brake();
+    void AddPassengers(int count);
+    void Dashboard();
+
+};
