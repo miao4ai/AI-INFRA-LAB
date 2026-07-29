160608964aea4b3772ae68e138cffb5264994d04
Miao Jiang <miao@zeustellus.com>
2026-07-28 22:51:35 -0700

    solve the problem in a C++ 20 way

--- cpp-lab/11_coding_practice/lc485/src/solution.cpp
+++ cpp-lab/11_coding_practice/lc485/src/solution.cpp
@@ -1,6 +1,40 @@
 #include "solution.hpp"
 
+/** 
 int findMaxConsecutiveOnes(const std::vector<int>& nums)
 {
-    // TODO
+    int ans = 0;
+    int count = 0;
+    int size = nums.size();
+    if(nums.size()==0){
+        return 0;
+    }
+
+    for (int i = 0; i<size; ++i){
+        if(nums[i]==1){
+            count++;
+            if(count>ans){
+                ans = count;
+            }
+        }
+        else{
+            count = 0;
+        }
+
+    }
+    return ans;
+}
+*/
+
+int findMaxConsecutiveOnes(const std::vector<int>& nums)
+{
+  int best = 0;
+  int run = 0;
+
+  for (int n:nums){
+    run = (n==1)? run+1:0;
+    best = std::max(best,run);
+  }
+  
+  return best;
 }
