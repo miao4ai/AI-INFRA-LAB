f256396ed9a88580a0ca2dcfdac0198728f39428
Miao Jiang <miao@zeustellus.com>
2026-08-04 21:46:33 -0700

    Initialize the class

--- cpp-lab/03_oop/copy_constructor/src/interger.h
+++
@@ -1,8 +0,0 @@
-class Interger{
-    int *m_pInt;
-public:
-    Interger();
-    Interger(int value);
-    int GetValue() const;
-    void SetValue(int value);
-};
---
+++ cpp-lab/03_oop/copy_constructor/src/main.cpp
@@ -0,0 +1,8 @@
+#include "integer.h"
+#include <iostream>
+
+using namespace std;
+int main(){
+    Integer i(5);
+    cout<< i.GetValue()<<endl;    
+}
