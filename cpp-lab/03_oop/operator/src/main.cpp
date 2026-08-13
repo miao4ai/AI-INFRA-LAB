7ad72d6a90d3d048b66c993094ca6a2e239ded4f
Miao Jiang <miao@zeustellus.com>
2026-08-12 23:08:35 -0700

    modify

--- cpp-lab/03_oop/operator/src/main.cpp
+++ cpp-lab/03_oop/operator/src/main.cpp
@@ -1,11 +1,23 @@
 #include "integer.h"
 #include <iostream>
 
+Integer operator +(int x, const Integer &y){
+    Integer temp;
+    temp.SetValue(x+y.GetValue());
+    return temp;
+}
+
+std::ostream & operator <<(std::ostream &out, const Integer &b){
+    out << b.GetValue();
+    return out;
+}
 int main(){
-    Integer a(1),b(3);
-    Integer c;
-    c=a;
+    //Integer a(1),b(3);
+    //Integer c;
+    //c=a;
+    Integer a(1);
+    Integer sum1 = a + 5;
     //Integer sum = a+b;
-    std::cout<<c.GetValue()<<std::endl;
+    std::cout<<sum1.GetValue()<<std::endl;
     return 0;
 }
